d41e8a74219a315118e5c115d54aa83fe4379056
xiaoyuer <605451377@qq.com>
2017-03-03 20:35:21 +0800

    调用Json显示地震内容

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -17,7 +17,6 @@ package com.example.android.quakereport;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -32,14 +31,7 @@ public class EarthquakeActivity extends AppCompatActivity {
         setContentView(R.layout.earthquake_activity);
 
         // Create a fake list of earthquake locations.
-        ArrayList<Earthquake> earthquakes = new ArrayList<>();
-        earthquakes.add(new Earthquake("7.2", "San Francisco", "Feb 2, 2016"));
-        earthquakes.add(new Earthquake("6.1", "London", "July 20, 2015"));
-        earthquakes.add(new Earthquake("3.9", "Tokyo", "Nov 10, 2014"));
-        earthquakes.add(new Earthquake("5.4", "Mexico City", "May 3, 2014"));
-        earthquakes.add(new Earthquake("2.8", "Moscow", "Jan 31, 2013"));
-        earthquakes.add(new Earthquake("4.9", "Rio de Janeiro", "Äug, 19, 2012"));
-        earthquakes.add(new Earthquake("1.6", "Paris", "Oct 31, 2011"));
+        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
